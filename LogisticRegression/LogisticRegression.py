34f2a49b281d40d95b38298bd33b6a44d4714f9a
SlimJesus101 <frankierong101@gmail.com>
2025-04-17 16:54:47 +0100

    Finished logistic regression

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,21 +1,44 @@
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import os
+
 script_dir = os.path.dirname(__file__)
-file_path = os.path.join(script_dir, "titanic_data.csv")
+file_path = os.path.join(script_dir, 'titanic_data.csv')
 df = pd.read_csv(file_path)
 
-##Cleaning
-common = df["Embarked"].mode()[0]
-df["Embarked"] = df["Embarked"].fillna(common)
-avg = df["Age"].mean()
-df["Age"] = df["Age"].fillna(avg)
-df = df.drop('Cabin', axis=1)
-df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
-df = pd.get_dummies(df, columns=["Embarked"], drop_first=True)
+## Cleaning
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
+df['Age'] = df['Age'].fillna(df['Age'].mean())
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+df = pd.get_dummies(df, columns=['Embarked'], drop_first=True)
+# df['FamilySize'] = df['Sibsp'] + df['Parch'] accuracy -= 0.01 when used
 
-##Checks made when cleaning
-#print(df["Embarked"].unique())
-#print(df.isna().sum())
-#print(df.columns.tolist())
-#print(df.duplicated().sum())
+## Checks made when cleaning
+# print(df["Embarked"].unique())
+# print(df.isna().sum())
+# print(df.columns.tolist())
+# print(df.duplicated().sum())
+
+## Features & Target
+x = df[['Pclass','Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked_Q', 'Embarked_S']]
+y = df['Survived']
+
+## Splitting
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2004)
+
+## Standardisation doesn't affect the accuracy at all
+# scaler = StandardScaler()
+# x_train_scaled = scaler.fit_transform(x_train)
+# x_test_scaled = scaler.transform(x_test)
+
+## Fitting
+model = LogisticRegression(max_iter=1000)
+model.fit(x_train, y_train)
+
+## Accuracy Check
+y_prediction = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_prediction)
+print(f"Accuracy: {accuracy:.2f}")
